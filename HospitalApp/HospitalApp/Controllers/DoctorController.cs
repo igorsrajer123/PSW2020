1c47c9168481b1d4358c6be38c5afe6ced4ab148
igorsrajer123 <igorsrajer123@gmail.com>
2020-11-29 21:29:00 +0100

    Initial app added

---
+++ HospitalApp/HospitalApp/Controllers/DoctorController.cs
@@ -0,0 +1,30 @@
+﻿using HospitalApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DoctorController : Controller
+    {
+        private readonly MyDbContext _dbContext;
+
+        public DoctorController(MyDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet("/getAllDoctors")]
+        public IActionResult Get()
+        {
+            List<Doctor> result = new List<Doctor>();
+            _dbContext.Doctors.ToList().ForEach(doctor => result.Add(doctor));
+
+            return Ok(result);
+        }
+    }
+}
---
+++ HospitalApp/HospitalApp/Models/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalApp.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+    }
+}
